23a1820ee227f6fd4bfa12bb4ede45614472b25c
pear <proxybomb@gmail.com>
2018-01-13 17:15:11 +0100

    dadadadada

--- bot.js
+++ bot.js
@@ -5,8 +5,8 @@ bot.on("ready", () => {
     console.log("I am ready!");
 });
 
-bot.on('message', function(message) {
-        bot.reply(message, "Hello!");
+bot.on("message", function(message) {
+        bot.sendMessage(message, "Hello!");
 });
 
 bot.login(process.env.BOT_TOKEN);
