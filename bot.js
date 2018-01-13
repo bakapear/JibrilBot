55b7f18ecc8e4f5f7839d5dfe1673f755dff8c06
pear <proxybomb@gmail.com>
2018-01-13 21:46:30 +0100

    quack

--- bot.js
+++ bot.js
@@ -1,18 +1,21 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const cfg = require("./config.json");
+if(process.env.BOT_TOKEN != undefined) {
+	client.login(process.env.BOT_TOKEN);
+}
+else { //for local testing
+	client.login(require("./config.json").token);
+}
 
 client.on("ready", () => {
-    console.log(process.env.BOT_TOKEN);
+	console.log("I'm ready oWo!");
 });
 
 client.on("message", (message) => {
 	if (message.content === ".ping") {
-		message.channel.send("testingu!");
+		message.channel.send("Pong!");
 	}
 	else if (message.content === ".avatar") {
 		message.channel.send(message.author.avatarURL);
 	}
 });
-
-client.login(cfg.token);
