46b3bff07ad62af113ee10dad7ad5dd66df3fd66
pear <proxybomb@gmail.com>
2018-01-13 17:43:39 +0100

    dadada

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
     console.log("I am ready!");
-	client.user.setGame("with master");
+	client.user.setPresence({game:{name:"with master",type:0}});
 });
 
 client.login(process.env.BOT_TOKEN);
