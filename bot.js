0c324a7d8e381e013552a326a2420ec6d170958a
pear <proxybomb@gmail.com>
2018-01-13 17:02:56 +0100

    commercial commit wait no

--- bot.js
+++ bot.js
@@ -10,26 +10,10 @@ bot.on("message", message => {
     	message.reply("Pong!");
   	}
 	else if (message.content === ".rnd") {
-		message.reply(Math.round(Math.random() * (100 - 1) + 1));
+		message.reply(Math.round(Math.floor() * (101 - 1) + 1));
 	}
 	else if (message.content === ".quack") {
-		switch(Math.round(Math.random() * (5 - 1) + 1)) {
-			case 1:
-				message.reply("Here, I brought you lunch. I-its not like I made it especially for you or anything, I just had some left over, okay!? Idiot!");
-				break;
-			case 2:
-				message.reply("OH MY GOD IM SEEING BOTH THE JAPANESE AND US VOICE ACTORS FOR ASUKA LANGLEY ON THE WEEKEND!!!!!!!");
-				break;
-			case 3:
-				message.reply("B-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-b-baka!");
-				break;
-			case 4:
-				message.reply("I'm running out of ideas!");
-				break;
-			case 5:
-				message.reply("I consider your class of demon very worthy, worthy to fight, and worthy to die!");
-				break;
-		}
+		bot.sendMessage(message.channel, "Hello!");
 	}
 });
 
