a90368be8f19444da7408a72f616193c5d71d020
pear <proxybomb@gmail.com>
2018-01-13 18:19:07 +0100

    dadadadada

--- bot.js
+++ bot.js
@@ -13,8 +13,8 @@ client.on("message", (message) => {
 	else if (message.content === ".avatar") {
 		message.channel.send(message.author.avatarURL);
 	}
-	else if (message.content === ".channel") {
-		message.channel.send(message.channel);
+	else if (message.content === ".nani") {
+		message.channel.send("${user}??????!!! DID YOU REALLY SAY THAT=!!?!!");
 	}
 });
 
