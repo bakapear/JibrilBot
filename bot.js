cceea3bdd54fd65d0b7ce2792620518e95c54fe3
pear <proxybomb@gmail.com>
2018-01-13 17:41:11 +0100

    back to da start

--- bot.js
+++ bot.js
@@ -3,11 +3,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
     console.log("I am ready!");
-	client.user.setGame("on ${client.guilds.size} servers");
-});
-
-client.on("message", function(message) {
-        client.sendMessage(message, "Hello!");
+	client.user.setGame("with master");
 });
 
 client.login(process.env.BOT_TOKEN);
