efcd11280c6f83f1046a03426a85db499aafc9c2
bakapear <proxybomb@gmail.com>
2018-01-25 00:28:47 +0100

    aw yis

--- core.js
+++ core.js
@@ -277,7 +277,7 @@ bot.on("message", msg => {
 				return;
 			}
 			request({
-				url: `https://api.qwant.com/api/search/images?count=10&safesearch=1&locale=en_US&q=${encodeURIComponent(msg.content.slice(cmd.length + 1).trim())}`,
+				url: `https://api.qwant.com/api/search/images?count=50&safesearch=1&locale=en_US&q=${encodeURIComponent(msg.content.slice(cmd.length + 1).trim())}`,
 				headers: {
 			        "User-Agent" : "Jibril"
 			    },
