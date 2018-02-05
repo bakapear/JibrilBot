c7e0d3e01d7f5759f69461e484f902c30c7704e5
pear <proxybomb@gmail.com>
2018-02-05 21:04:42 +0100

    added a counter cuz i need it

---
+++ cmds/counter.js
@@ -0,0 +1,33 @@
+const core = require("../core.js");
+const boot = core.boot;
+let timer = {};
+
+module.exports = {
+    name: ["counter"],
+    desc: "Starts counting up.",
+    permission: "",
+    usage: "(reset)",
+    args: 0,
+    command: function (msg, cmd, args) {
+        if (!timer.hasOwnProperty(msg.author.id)) timer[msg.author.id] = 0;
+        if (args == "") {
+            if (timer[msg.author.id] == 0) {
+                timer[msg.author.id] = Date.now();
+                msg.channel.send(`Counter started!`);
+            }
+            else {
+                const elapsed = new Date(Date.now() - timer[msg.author.id]);
+                msg.channel.send(`${elapsed.getSeconds()} and counting...`);
+            }
+        }
+        if (args[0] == "reset") {
+            if (timer[msg.author.id] == 0) {
+                msg.channel.send("No counter is running!");
+            }
+            else {
+                timer[msg.author.id] = 0;
+                msg.channel.send("Counter has been reset!");
+            }
+        }
+    }
+}
