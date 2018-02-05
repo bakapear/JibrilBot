02ff209c1906384e2dbf2fb0ccb259a2abe1403a
pear <proxybomb@gmail.com>
2018-02-05 10:01:27 +0100

    ignore this for now xd

--- cmds/test.js
+++ cmds/test.js
@@ -1,14 +1,35 @@
+const fs = require('fs');
+
 module.exports = {
-    name: ["test"],
+    name: ["tea"],
     desc: "Does some test stuff",
     permission: "",
     usage: "",
     args: 0,
     command: function (msg, cmd, args) {
-        msg.channel.send({
-            embed: {
-                description: args[0]
-            }
+        msg.member.voiceChannel.join().then(connection => {
+            msg.reply("Ready!");
+            const receiver = connection.createReceiver();
+            let stream;
+            connection.on("speaking", (user, speaking) => {
+                if(speaking) {
+                    msg.channel.send(`${user} speaking!`);
+                    stream = receiver.createOpusStream(user);
+                }
+                else {
+                    msg.channel.send(`${user} no longer speaking!`);
+                    if(stream != undefined) {
+                        let player = connection.playOpusStream(stream);
+                        player.setBitrate(96000);
+                        player.on("end", () => {
+							msg.channel.send("STOP!");
+						});
+						player.on("error", e => {
+							console.log(e);
+						});
+                    }
+                }
+            });
         });
     }
 }
