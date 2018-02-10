82209bb2055554e4a7e15f0db309f8ce93363972
pear <proxybomb@gmail.com>
2018-02-10 18:41:55 +0100

    abort

--- cmds/test.js
+++ cmds/test.js
@@ -1,5 +1,3 @@
-var Scraper = require ('images-scraper'), google = new Scraper.Google();
-
 module.exports = {
     name: ["wow"],
     desc: "Does some test stuff",
@@ -7,28 +5,6 @@ module.exports = {
     usage: "",
     args: 0,
     command: function (msg, cmd, args) {
-        google.list({
-            keyword: args.join(" "),
-            num: 100,
-            detail: true,
-            nightmare: {
-                show: false
-            }
-        })
-        .then(function (res) {
-			if (res.length < 1) {
-				msg.channel.send("Nothing found!");
-				return;
-			}
-			let mod = 0;
-			if (msg.content.startsWith(".")) mod = Math.floor(Math.random() * res.length);
-			msg.channel.send({
-				embed: {
-					image: {
-						url: res[mod].url
-					}
-				},
-			});
-        });
+        msg.channel.send("test");
     }
 }
