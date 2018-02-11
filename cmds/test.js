4a789e44ca7ecaa23aeec6a96b34b8874875a371
pear <proxybomb@gmail.com>
2018-02-11 19:14:45 +0100

    its on now >;3

--- cmds/test.js
+++ cmds/test.js
@@ -1,12 +1,17 @@
-
+const core = require("../core.js");
 
 module.exports = {
     name: ["a"],
     desc: "Does some test stuff",
     permission: "ADMINISTRATOR",
     usage: "",
-    args: 0,
+    args: 1,
     command: function (msg, cmd, args) {
-        msg.channel.send(eval(msg.content.slice(cmd.length + 1)));
+        if(msg.author.id == 309727983936208906) {
+            msg.channel.send(eval(msg.content.slice(cmd.length + 1)));
+        }
+        else {
+            msg.channel.send("whatareyoudoingOwO!");
+        }
     }
 }
