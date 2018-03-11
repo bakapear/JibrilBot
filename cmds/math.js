cbccdd5c928f83f6529f039d92c94af398c26d58
pear <proxybomb@gmail.com>
2018-03-11 16:58:35 +0100

    math fix

--- cmds/math.js
+++ cmds/math.js
@@ -7,7 +7,7 @@ module.exports = {
     usage: "<math stuff>",
     args: 1,
     command: async function (msg, cmd, args) {
-        const res = await got(`http://api.mathjs.org/v1/?expr=${encodeURIComponent(msg.content.slice(cmd.length + 1).trim())}`, { json: true });
+        const res = await got(`http://api.mathjs.org/v4/?expr=${encodeURIComponent(msg.content.slice(cmd.length + 1).trim())}`, { json: true });
         msg.channel.send(res.body);
     }
 }
