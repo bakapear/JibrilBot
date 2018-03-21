aed36b37f1b54c553b5da44ba16a8bdc96fa12db
pear <proxybomb@gmail.com>
2018-03-21 16:32:24 +0100

    fixed numfact

--- cmds/numfact.js
+++ cmds/numfact.js
@@ -4,21 +4,20 @@ module.exports = {
     name: ["numfact"],
     desc: "Displays a number fact!",
     permission: "",
-    usage: "(trivia/date/year/math) (number)",
+    usage: "(number) (trivia/date/year/math)",
     args: 0,
     command: async function (msg, cmd, args) {
         let link;
         let search = "random";
-        if (args[1]) search = args[1];
-        switch (args[0]) {
+        if (args[0]) search = args[0];
+        switch (args[1]) {
             case "trivia": { link = `http://numbersapi.com/${search}/trivia`; break; }
             case "date": { link = `http://numbersapi.com/${search}/date`; break; }
             case "year": { link = `http://numbersapi.com/${search}/year`; break; }
             case "math": { link = `http://numbersapi.com/${search}/math`; break; }
-            case undefined: { link = `http://numbersapi.com/random/`; break; }
-            default: { msg.channel.send("Invalid argument!"); return; }
+            default: { link = `http://numbersapi.com/${search}`; break; }
         }
-        const body = (await got(link, { json: true })).body;
+        const body = (await got(link)).body;
         if (body.startsWith("Cannot GET") || body.startsWith("Invalid url")) { msg.channel.send("Nothing found!"); return; }
         msg.channel.send(body);
     }
