e2dc1d544d2956858f701bb943446fce39935cd8
pear <proxybomb@gmail.com>
2018-03-22 14:52:55 +0100

    more types for leet

--- cmds/leet.js
+++ cmds/leet.js
@@ -2,12 +2,19 @@ const got = require("got");
 
 module.exports = {
     name: ["leet","1337"],
-    desc: "Converts your text into leetspeak m8.",
+    desc: "Converts your text into leetspeak m8. Types: classic, ultra, glyphtek, wolfenstein, fancy, beefy, broadway, fullwidth",
     permission: "",
-    usage: "",
+    usage: "(type) ; <text>",
     args: 1,
     command: async function (msg, cmd, args) {
-        const body = (await got(`http://api.genr8rs.com/Generator/Fun/LeetSpeakGenerator?_sText=${encodeURIComponent(msg.content.slice(cmd.length + 1).trim())}&_sCharacterSet=classic`, { json: true })).body;
+        let mode = "classic"
+        if(args[1] == ";") {
+           mode = args[0];
+           args.splice(0,2);
+        }
+        let types = ["classic", "ultra", "glyphtek", "wolfenstein", "fancy", "beefy", "broadway", "fullwidth"];
+        if(!types.includes(mode)) { msg.channel.send("Invalid type!"); return; }
+        const body = (await got(`http://api.genr8rs.com/Generator/Fun/LeetSpeakGenerator?_sText=${encodeURIComponent(args.join(" ").trim())}&_sCharacterSet=${mode}`, { json: true })).body;
         msg.channel.send(body._sResult);
     }
 }
