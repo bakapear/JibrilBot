285ae21074d770c7775d4914fa4822d7e22b7a00
pear <proxybomb@gmail.com>
2018-03-22 10:40:19 +0100

    writing more stuff on phone ez

---
+++ cmds/taunt.js
@@ -0,0 +1,20 @@
+const got = require("got");
+
+module.exports = {
+    name: ["taunt"],
+    desc: "Says a random game taunt.",
+    permission: "",
+    usage: "(polite|funny|rude)",
+    args: 0,
+    command: async function (msg, cmd, args) {
+        let mode = "polite";
+        switch(args[0]) {
+            case "polite": {mode = "polite"; break;}
+            case "funny": {mode = "funny"; break;}
+            case "rude": {mode = "rude"; break;}
+            default: {msg.channel.send("Invalid type!"); return;}
+        }
+        const body = (await got(`http://api.genr8rs.com/Generator/Fun/GameTauntGenerator?_sInsultLevel=${mode}`, {json: true })).body;
+        msg.channel.send(body[0]);
+    }
+}
