66a7b1df31f0455662393ed951f911737430284e
pear <proxybomb@gmail.com>
2018-03-24 10:12:00 +0100

    added hidden lofi function

--- cmds/radio.js
+++ cmds/radio.js
@@ -1,3 +1,5 @@
+const yt = require("ytdl-core");
+
 module.exports = {
 	name: ["radio", "r"],
 	desc: "Streams the listen.moe anime radio in the voicechannel.",
@@ -11,16 +13,31 @@ module.exports = {
 		if (voiceq[msg.guild.id].playing == 0) {
 			msg.member.voiceChannel.join().then(connection => {
 				voiceq[msg.guild.id].playing = 2;
-				msg.channel.send({
-					embed: {
-						color: 14506163,
-						title: "Radio",
-						description: `Joined ${connection.channel} streaming *listen.moe*!`
-					}
-				});
-				let radio;
-				radio = connection.playArbitraryInput(`https://listen.moe/opus`);
-				radio.setBitrate(96000);
+				if (args[0] == "lofi") {
+					msg.channel.send({
+						embed: {
+							color: 14506163,
+							title: "Radio",
+							description: `Joined ${connection.channel} streaming *lofi hip hop radio*!`
+						}
+					});
+					let radio;
+					let stream = yt("https://www.youtube.com/watch?v=8XiqrERZo_8", { range: { starts: 999999 } });
+					radio = connection.playStream(stream);
+					radio.setBitrate(96000);
+				}
+				else {
+					msg.channel.send({
+						embed: {
+							color: 14506163,
+							title: "Radio",
+							description: `Joined ${connection.channel} streaming *listen.moe*!`
+						}
+					});
+					let radio;
+					radio = connection.playArbitraryInput(`https://listen.moe/opus`);
+					radio.setBitrate(96000);
+				}
 			});
 		}
 		else {
