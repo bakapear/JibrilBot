270508c252fdaf5f72d0af7506b26588fffa4551
pear <proxybomb@gmail.com>
2018-03-25 15:52:15 +0200

    play just stay play

--- cmds/play.js
+++ cmds/play.js
@@ -4,7 +4,7 @@ const api_google = process.env.API_GOOGLE;
 let dispatcher;
 
 module.exports = {
-    name: ["pla"],
+    name: ["play"],
     desc: "Streams audio from a youtube video into the voicechannel.",
     permission: "",
     usage: "<query|url>",
