839f02cc5022552b33a4a828925c62d060546f33
bakapear <proxybomb@gmail.com>
2018-04-22 21:51:54 +0200

    this one too!

--- cmds/rhyme.js
+++ cmds/rhyme.js
@@ -7,7 +7,7 @@ module.exports = {
     usage: "<word>",
     args: 1,
     command: async function (msg, cmd, args) {
-        let url = "https://api.datamuse.com/words?rel_rhy=" + encodeURIComponent(msg.content.slice(cmd.length + 1).trim());
+        let url = "https://api.datamuse.com/words?v=enwiki&rel_rhy=" + encodeURIComponent(msg.content.slice(cmd.length + 1).trim());
         let body = (await got(url, { json: true })).body;
         if (body.length < 1) { msg.channel.send("Nothing found!"); return; }
         let wordlist = "";
