89190916c77ca94451271cd8e71515f56c2eabc7
bakapear <proxybomb@gmail.com>
2018-04-27 18:25:39 +0200

    queue only shows 15 items max now

--- cmds/queue.js
+++ cmds/queue.js
@@ -2,19 +2,20 @@ module.exports = {
     name: ["q", "queue"],
     desc: "Shows the entire queue or if a number is given, the song at that position.",
     permission: "",
-    usage: "(position)",
+    usage: "(position) (rem)",
     args: 0,
     command: function (msg, cmd, args) {
         if (!voiceq.hasOwnProperty(msg.guild.id) || !voiceq[msg.guild.id].songs.length) { msg.channel.send("No songs in queue!"); return; }
         let songnames = [];
         if (args == "") {
-            for (i = 0; i < voiceq[msg.guild.id].songs.length; i++) {
+            let len = voiceq[msg.guild.id].songs.length > 15 ? 15 : voiceq[msg.guild.id].songs.length;
+            for (i = 0; i < len; i++) {
                 let song = voiceq[msg.guild.id].songs[i][1];
-                if (song.length > 50) song = song.substring(0, 50) + "...";
                 let num = i + ".";
                 if (num == 0) num = "NP:"
                 songnames.push(`${num} \`${song}\`\n`);
             }
+            if (voiceq[msg.guild.id].songs.length > 15) songnames.push(`...`);
         }
         else {
             if (isNaN(args[0])) { msg.channel.send("Please enter a number!"); return; }
@@ -31,7 +32,7 @@ module.exports = {
         msg.channel.send({
             embed: {
                 color: 14506163,
-                title: "Play Queue",
+                title: "Play Queue - " + voiceq[msg.guild.id].songs.length + " Items",
                 description: songnames.join("").substring(0, 2045)
             }
         });
