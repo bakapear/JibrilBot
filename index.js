8bd1b412b37b6ec01e3e2a0ac66c430ffea0bf24
bakapear <proxybomb@gmail.com>
2018-11-03 15:14:51 +0100

    this can take some TIME get it

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ module.exports = require("./lib/core")
 
 function t(s) {
     let d = Math.floor(s / (3600 * 24))
-    let z = new Date(s * 1000).toISOString().substr(11, 8)
-    return `${d} Days ${z}`
+    let a = new Date(s * 1000).toISOString().substr(11, 8)
+    return `${d} Days ${a}`
 }
