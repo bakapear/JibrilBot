4b47fda74f1ac8c24172d0ca67448c894a3d86b5
bakapear <proxybomb@gmail.com>
2019-07-14 19:49:55 +0200

    added musicplayer module

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 #### The youngest and strongest Discord Bot of the Flügel race.
 
 Yo, this is Jibs. She's a Discord Bot I made for my Discord Server and I'm currently still developin' her. <br>
-Jibril is hosted completely free on [zeit.co](https://zeit.co/) and is written in [discord.js](https://github.com/discordjs/discord.js) following the [standard style](https://github.com/standard/standard). <br>
+Jibril is hosted completely free on [zeit.co](https://zeit.co/) and is written in [discord.js](https://github.com/discordjs/discord.js) following the [standard style](https://github.com/standard/standard) (disabled prefer-const though). <br>
 
 Here's her website if you want to CHECK THE UPTIME: [website](https://jibril.now.sh/) (more stuff comming soon)
 
--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-setTimeout(() => require('child_process').exec(`now alias rm jibril --yes --token ${process.env.TOKEN} ; now alias --token ${process.env.TOKEN} ; now rm jibril --safe --yes --token ${process.env.TOKEN}`, (e, out, err) => {
-  if (err) console.log(err)
-  if (out) console.log(out)
-}), 10000)
-
 let server = require('server')
 let { get, error } = server.router
 let { json, status } = server.reply
 
-let getData = require('./lib/core')
+module.exports = require('./lib/core')
 
 server({ public: 'views' }, [
   get('/up', ctx => json(t(process.uptime()))),
-  get('/store', ctx => json(getData())),
+  get('/store', ctx => json({})),
   get('/favicon.ico', ctx => status(200)),
   get('/*', ctx => 'Why are you even here'),
   error(ctx => console.error(ctx.error))
