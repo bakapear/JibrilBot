398e5479ee08acbc0b2090d7ebb0b3e342ae282b
bakapear <proxybomb@gmail.com>
2019-07-14 19:56:59 +0200

    moving to glitch

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+let http = require('http')
 let server = require('server')
 let { get, error } = server.router
 let { json, status } = server.reply
 
-module.exports = require('./lib/core')
-
-server({ public: 'views' }, [
+server({ port: process.env.PORT, public: 'views' }, [
   get('/up', ctx => json(t(process.uptime()))),
   get('/store', ctx => json({})),
   get('/favicon.ico', ctx => status(200)),
@@ -12,8 +11,12 @@ server({ public: 'views' }, [
   error(ctx => console.error(ctx.error))
 ])
 
+if (!process.env.DEBUG) setInterval(() => http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`, 280000))
+
 function t (s) {
   let d = Math.floor(s / (3600 * 24))
   let a = new Date(s * 1000).toISOString().substr(11, 8)
   return `${d} Days ${a}`
 }
+
+module.exports = require('./lib/core')
