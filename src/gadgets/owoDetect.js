3490d1649dee13d491e9fe91d2bf0d5fd4a7e474
bakapear <proxybomb@gmail.com>
2019-07-31 17:02:52 +0200

    FIX

--- src/gadgets/owoDetect.js
+++ src/gadgets/owoDetect.js
@@ -2,6 +2,7 @@ module.exports = {
   disabled: false,
   type: 'msg',
   check: async msg => {
+    if (msg.author.bot) return false
     if (msg.content.indexOf('owo') >= 0) {
       msg.channel.send(`Oh noe, did <@${msg.author.id}> just do the forbidden OWO?!`)
     }
