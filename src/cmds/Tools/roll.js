2bd65c757bd4c780d3761d5224d8a7c3518ca1e0
bakapear <proxybomb@gmail.com>
2019-08-14 20:13:41 +0200

    smol fix

--- src/cmds/Tools/roll.js
+++ src/cmds/Tools/roll.js
@@ -7,10 +7,10 @@ module.exports = {
   command: async function (msg, cmd, args) {
     let max = 6
     let min = 1
-    if (args.length === 1) max = parseInt(args[0])
+    if (args.length === 1) max = parseInt(args[0]) || 6
     else if (args.length > 1) {
-      min = parseInt(args[1])
-      max = parseInt(args[0])
+      min = parseInt(args[1]) || 1
+      max = parseInt(args[0]) || 6
     }
     let rnd = Math.floor(Math.random() * (max - min + 1)) + min
     msg.channel.send(`:game_die: ${msg.author.username} rolled a **${rnd}**!`)
