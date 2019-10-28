10a3663c25d824e545ba1251e482cfcdceb2d1f9
bakapear <proxybomb@gmail.com>
2019-10-28 16:10:39 +0100

    fixed tfitems

--- src/cmds/Services/tfitem.js
+++ src/cmds/Services/tfitem.js
@@ -9,32 +9,25 @@ module.exports = {
   args: 0,
   command: async function (msg, cmd, args) {
     try {
-      let items = await getItems(args.join(' ').trim(), msg.content.startsWith(cfg.prefix.first))
-      if (items === -1) {
-        msg.channel.send('Please enter a name with at least 3 characters or more.')
-        return
-      }
-      if (!items.count) {
-        msg.channel.send('Nothing found!')
-        return
-      }
-      let mod = msg.content.startsWith(cfg.prefix.random) ? Math.floor(Math.random() * items.matches.length) : 0
-      if (args.join(' ').trim() === '') mod = Math.floor(Math.random() * items.matches.length)
-      let item = items.matches[mod]
+      let items = await getItems(args.join(' ').trim(), msg.content.startsWith(cfg.prefix.random))
+      if (!items) return msg.channel.send('Something went wrong!')
+      if (!items.length) return msg.channel.send('Nothing found!')
+      let mod = msg.content.startsWith(cfg.prefix.random) ? Math.floor(Math.random() * items.length) : 0
+      if (args.join(' ').trim() === '') mod = Math.floor(Math.random() * items.length)
+      let item = items[mod]
       let attribs = []
-      if (item.attribs) {
-        if (item.attribs.neutral) attribs.push({ name: 'Neutral Attribs', value: item.attribs.neutral.join('\n') })
-        if (item.attribs.positive) attribs.push({ name: 'Positive Attribs', value: item.attribs.positive.join('\n') })
-        if (item.attribs.negative) attribs.push({ name: 'Negative Attribs', value: item.attribs.negative.join('\n') })
+      if (item.attributes) {
+        if (item.attributes.unusual) attribs.push({ name: 'Unusual Attribs', value: item.attributes.unusual.join('\n') })
+        if (item.attributes.neutral) attribs.push({ name: 'Neutral Attribs', value: item.attributes.neutral.join('\n') })
+        if (item.attributes.positive) attribs.push({ name: 'Positive Attribs', value: item.attributes.positive.join('\n') })
+        if (item.attributes.negative) attribs.push({ name: 'Negative Attribs', value: item.attributes.negative.join('\n') })
       }
       let desc = item.desc.replace(/\\n/g, '\n')
       msg.channel.send({
         embed: {
           title: item.name,
           description: item.level + '\n\n' + desc,
-          thumbnail: {
-            url: item.img
-          },
+          thumbnail: { url: item.img },
           fields: attribs
         }
       })
@@ -44,13 +37,14 @@ module.exports = {
   }
 }
 
-async function getItems (query, weps) {
-  let search = query
-  if (!query) search = ''
-  else if (isNaN(query) && query.length < 3) return -1
-  let opts = { q: search }
-  if (weps && query === '') opts.weps = 1
-  let url = 'https://purr.now.sh/tfitems/search'
-  let body = (await got(url, { query: opts, json: true })).body
-  return body
+async function getItems (query, rnd) {
+  try {
+    if (!query) {
+      if (rnd) query = 'type!=medal'
+      else query = '*'
+    }
+    let url = 'https://purr.now.sh/tfitems/items'
+    let { body } = await got(url, { query: { q: query }, json: true })
+    return body
+  } catch (e) { return null }
 }
